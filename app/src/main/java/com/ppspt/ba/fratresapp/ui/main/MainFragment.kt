6fcd4325e96194c1ae6cc6b88ad36778a2058064
Giuseppe Fabio Trentadue <gf.trentadue@gmail.com>
2019-10-27 13:08:46 +0100

    Change style and add calendar, toolbar to first fragment layout.

--- app/src/main/java/com/ppspt/ba/fratresapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/ppspt/ba/fratresapp/ui/main/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.ppspt.ba.fratresapp.R
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
 
@@ -26,7 +27,6 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
