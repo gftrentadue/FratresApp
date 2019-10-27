4152502d0629f0cb3653baa84423ac057583e52f
Giuseppe Fabio Trentadue <gf.trentadue@gmail.com>
2019-10-27 12:42:23 +0100

    Add support to Google Material Design.

--- app/src/main/java/com/ppspt/ba/fratresapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/ppspt/ba/fratresapp/ui/main/MainFragment.kt
@@ -1,11 +1,11 @@
 package com.ppspt.ba.fratresapp.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.ppspt.ba.fratresapp.R
 
 class MainFragment : Fragment() {
