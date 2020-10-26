b0d92fd142d9bc3f25d217e69c7b10ba36234967
Giuseppe Fabio Trentadue <giuseppe.trentadue@si2001.it>
2020-10-26 15:22:44 +0100

    Connect main navigation with botton navigation bar.

--- app/src/main/java/com/ppspt/ba/fratresapp/MainActivity.kt
+++ app/src/main/java/com/ppspt/ba/fratresapp/MainActivity.kt
@@ -2,11 +2,18 @@ package com.ppspt.ba.fratresapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        val navController = Navigation.findNavController(this, R.id.navHostFragment)
+
+        bottomNavigationBar.setupWithNavController(navController)
     }
 }
